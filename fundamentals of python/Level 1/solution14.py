f710ced8c3ecbd41dd8a4230b62dff5a77a0b1e1
sajal1302 <50777789+sajal1302@users.noreply.github.com>
2020-01-21 14:08:03 +0530

    21.02.2020

---
+++ fundamentals of python/Level 1/solution14.py
@@ -0,0 +1,25 @@
+#PF-Tryout
+def find_five_digit():
+    for b in range(0,10):
+        a=b+2
+        c=b-2
+        d=b-4
+        e=c 
+        if (c+d+e==a) and (a+b+c+d+e==19):
+            a=str(a)
+            b=str(b)
+            c=str(c)
+            d=str(d)
+            e=str(e)
+            print(a+b+c+d+e)
+        
+            # TODO: write code...
+            # TODO: write code...
+    
+                            
+                    
+                    
+                
+	#start writing your code here
+
+find_five_digit()
