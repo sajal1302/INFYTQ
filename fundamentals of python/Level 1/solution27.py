0082300f2cf9194d73ac16b0cc77a95101b63344
sajal1302 <50777789+sajal1302@users.noreply.github.com>
2020-01-23 14:14:47 +0530

    23.01.2020

---
+++ fundamentals of python/Level 1/solution27.py
@@ -0,0 +1,7 @@
+def check_for_ten(num1,num2):
+    #start writing your code here
+    if (num1==10 or num2==10) or (num1+num2)==10:
+        return True
+    else:
+        return False
+print(check_for_ten(10,9))
